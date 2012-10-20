fb5100b2c512b80b19d3a9083fe6d9d879203858
Michael Belardo <mbelardo1@gmail.com>
2012-10-20 14:10:32 -0400

    created examples of data and a script. updated readme

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 #Boombot Scripts
 
-##Optional Scripts for [Boombot](https://github.com/TerrordactylDesigns/boombot)
+####Optional Scripts for [Boombot](https://github.com/TerrordactylDesigns/boombot)
 
-###For V2.0.0
+###For V2.0.0 which is still currently under development
+
+###About
+
+I wanted to make scripts a little more community driven. Every time I
+see the modifications people have done to my scripts it made me want to
+have a place to share everyones personal touches. Here it is.....
+
+###How to install
+
+* Phase 1 - download
+* Phase 2 - copy the scripts you want into your Boombot's scripts dir
+* Phase 3 - ????
+* Phase 4 - profit.
+
+###Write some of your own!
+
+example.script.js in the examples directory will point you to your new
+addiction... Each script will require the 3 exports and the header
+comments/information. An example of ttapi's data return that is sent
+in to the script function is also provided.
+
+###Fork me? Fork you!
+
+Made a script? Is it cool? Of course it is! So fork the damn repo and
+send me a pull request with your gift to the world. Go down in the
+history of Boombot as the guy who wrote that script that everyone loves!
---
+++ examples/example.script.js
@@ -0,0 +1,27 @@
+/**/// Description: I'm an example of how to make your own script!
+/**///
+/**/// Dependencies: None (if you require a package/import please tell us)
+/**///
+/**/// Author: https://github.com/TerrordactylDesigns (become famous!)
+/**///
+/**/// Notes: These comments here are all required information if you
+/**///        submit scripts to be published. Just give us the basics,
+/**///        so it's easiest on people new to your command.
+exports.trigger = '/example';
+exports.listed = true;
+exports.script = function(boombot, data) {
+  boombot.bot.speak('What a lame example!');
+}
+/*<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+  trigger - if your trigger has a / in front it will only trigger on
+            that exact match. If there is no / it will find that word
+            ANYWHERE in a chat sentence.
+
+  listed  - If set true the script trigger will be listed automatically
+            when /help is called. If false the script still runs, just
+            will not be added to your bots help command.
+
+  script  - What you came to do. an entire Boombot object is passed in.
+            Go wild. Along with a Boombot is the ttapi data return from
+            a speak event.
+<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>*/
