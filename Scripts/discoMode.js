a7d8d336f793a35c8c649ef5531cc8d63d8a6596
ZECTBynmo <michael.vegeto@gmail.com>
2012-11-26 18:36:46 -0500

    Disco mode!

---
+++ Scripts/discoMode.js
@@ -0,0 +1,36 @@
+/**/// Description: Turn on disco mode!
+/**/// Switches through available avatars once per second until
+/**/// disco mode is turned off (by repeating the command)
+/**///
+/**/// Dependencies: None
+/**///
+/**/// Author: https://github.com/ZECTBynmo
+/**///
+/**/// Notes: None
+var isDiscoOn = false,
+	iCurrentAvatar = 0,
+	discoInterval;
+
+exports.trigger = '/disco';
+exports.listed = true;
+exports.script = function( boombot, text, uname, uid, private ) {
+	isDiscoOn = !isDiscoOn;
+
+	if( typeof(discoInterval) == "undefined" ) {
+		discoInterval = setInterval( function() {
+			// Make sure disco mode is on
+			if( isDiscoOn ) {
+				// Get the list of available avatars
+				boombot.getAvatarIds( function(data) {
+					iCurrentAvatar++;
+					
+					if( iCurrentAvatar >= data.ids.length ) {
+						iCurrentAvatar = 0;
+					}
+					
+					boombot.setAvatar( iCurrentAvatar );
+				});
+			}
+		}, 1000);
+	}
+}
