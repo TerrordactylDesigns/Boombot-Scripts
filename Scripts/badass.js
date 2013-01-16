f9fc3f3907ff7f02f872476d80cf7d30789ce2cf
Michael Belardo <mbelardo1@gmail.com>
2013-01-15 23:15:47 -0500

    added badass script

---
+++ Scripts/badass.js
@@ -0,0 +1,16 @@
+/**/// Description: Listens for the word badass and returns a random badass meme
+/**///
+/**/// Dependencies: None
+/**///
+/**/// Author: https://github.com/TerrordactylDesigns
+/**///
+/**/// Notes: None
+exports.trigger = 'badass';
+exports.listed = true;
+exports.script = function(boombot, text, uname, uid, private) {
+  var badList = ['http://i3.kym-cdn.com/entries/icons/original/000/007/508/watch-out-we-got-a-badass-over-here-meme.png'
+                ,'Watch out, we got a badass over here....'
+                ,'http://weknowmemes.com/wp-content/uploads/2011/12/watch-out-badass-obi-over-here.png'];
+  var rndm = Math.floor(Math.random() * 3);
+  boombot.respond(uid, badList[rndm], private);
+}
